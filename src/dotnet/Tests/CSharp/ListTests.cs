41c899054943bd8237dc6169b5493b2b3aebadc2
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-08-24 09:45:29 +0200

    Setup demo

---
+++ src/dotnet/Tests/CSharp/ListTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AlgDat.Dotnet.Tests.CSharp
+{
+    public class ListTests
+    {
+        [Fact]
+        public void CanAddSingleElement()
+        {
+
+        }
+    }
+}
